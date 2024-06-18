18025b5a2affbcc98392bdf78c1ca4c904682dbd
Wesley-L <limawesley250@gmail.com>
2024-06-18 15:57:35 -0300

    Upload Organo

--- src/componets/Team/index.js
+++ src/componets/Team/index.js
@@ -10,13 +10,13 @@ const Team = (props) => {
         (props.collaborators.length > 0) ? 
         
         <section className='team' style={primaryStyle}>
-            <h3 style={secondaryStyle}>{props.nome}</h3>
+            <h3 style={secondaryStyle}>{props.name}</h3>
             <div className='collaborators'>
                 {props.collaborators.map( collaborator => 
                     <Collaborator 
                         corDeFundo={props.primaryColor} 
-                        key={collaborator.nome} 
-                        name={collaborator.nome} 
+                        key={collaborator.name} 
+                        name={collaborator.name} 
                         cargo={collaborator.cargo} 
                         image={collaborator.imagem}
                     />)}
